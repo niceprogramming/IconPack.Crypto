45ef06f4812001dcbbccc1b7e780eab42b40cc61
Adam Kallenbach <adamkallenbach@gmail.com>
2017-12-23 14:08:37 -0600

    Add PackIcon Files

---
+++ IconPack.Crypto/PackIcon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ControlzEx;
+
+namespace IconPack.Crypto
+{
+    /// <summary>
+    ///     Icons from Crypto Coins -
+    ///     <see>
+    ///         <cref>https://github.com/allienworks/cryptocoins</cref>
+    ///     </see>
+    ///     .
+    /// </summary>
+    public class PackIcon : PackIconBase<PackIconKind>
+    {
+        static PackIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PackIcon), new FrameworkPropertyMetadata(typeof(PackIcon)));
+        }
+
+        public PackIcon() : base(PackIconDataFactory.Create)
+        {
+        }
+    }
+}
