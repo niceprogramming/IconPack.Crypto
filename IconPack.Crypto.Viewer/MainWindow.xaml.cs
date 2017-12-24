929ff2f68cb8c3fc17416f73a500de10b5835252
Adam Kallenbach <adamkallenbach@gmail.com>
2017-12-23 20:34:07 -0600

    add in view program

--- IconPack.Crypto.Viewer/MainWindow.xaml.cs
+++ IconPack.Crypto.Viewer/MainWindow.xaml.cs
@@ -12,17 +12,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ReactiveUI;
 
 namespace IconPack.Crypto.Viewer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<MainWindowViewModel>
     {
         public MainWindow()
         {
+            ViewModel = new MainWindowViewModel();
             InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                d(this.OneWayBind(ViewModel, vm => vm.IconNames, view => view.IconListBox.ItemsSource));
+
+
+            });
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(nameof(ViewModel), typeof(MainWindowViewModel), typeof(MainWindow),
+                new PropertyMetadata(null));
+
+
+        public MainWindowViewModel ViewModel
+        {
+            get => (MainWindowViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = value as MainWindowViewModel;
         }
     }
 }
---
+++ IconPack.Crypto.Viewer/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IconPack.Crypto.Viewer
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+          IconNames = new ObservableCollection<string>(Enum.GetNames(typeof(PackIconKind))); 
+        }
+        public ObservableCollection<string> IconNames { get; set; }
+    }
+
+    
+}
